40975740ba6ae8f3f743098ffd021090432dd483
Luis Zambrano <lzambra2@yorku.ca>
2022-11-02 18:05:01 -0400

    Adding barrio + customized css.

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.york_drupal_theme = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
